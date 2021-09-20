1abc52840f5d044d918c741d9d05d06124c06099
nitik89 <82098445+nitik89@users.noreply.github.com>
2021-09-21 01:17:03 +0530

    Update cardekho.js

--- cardekho.js
+++ cardekho.js
@@ -1,24 +1,26 @@
-const fs = require('fs');
-let puppet=require('puppeteer');
-const PDFDocument = require("pdfkit");
-let xlsx = require("xlsx");
+const fs = require('fs'); //file module
+let puppet=require('puppeteer'); //for automation
+const PDFDocument = require("pdfkit"); //for using pdf 
+let xlsx = require("xlsx"); //for using excel
 let flag=true;
-let inp=process.argv.slice(2);
+let inp=process.argv.slice(2);  //inputs from user
 
-if(inp.length==0){
+if(inp.length==0){  //to check if no arguments are passed
 flag=false;
 console.log("Please enter the variables")
 }
-if(inp.length==1){
+
+
+if(inp.length==1){ //if only one argument is passed then the user is allowed to ask the query for the commands to be used
     flag=false;
-    if(inp[0]=='-q'){
+    if(inp[0]=='-q'){  //using q one can enter it
         console.log("This project is used to get details about the car\n Here you need to pass 5 variables\n1.Start Range \n2.End Range (Should be under 1Cr)  \n3.Type of car (suv,hatchback,sedan,muv) \n4.Sortlist on the basis of(avg,engine,price) \n5.Format of file (pdf,xls,json)");
     }
     else{
         console.log("Please select correct input")
     }
 }
-
+//the checking block starts
 if(inp.length==5){
     
     if(inp[0]<2){
@@ -42,7 +44,9 @@ if(inp.length==5){
         console.log("Please select a valid format type")
     }
 }
-if(flag){
+//the checking block ends
+
+if(flag){  // if everything is correct then go ahead
 
 (async function fn(){
     
@@ -55,50 +59,54 @@ if(flag){
             args:["--start-maximized","--disable-notifications"]
         
         })
-    browser=await browserStart;
+ browser=await browserStart;
  let page=await browser.newPage();
  let cpage=page;
- await cpage.goto(`https://www.cardekho.com/new-${inp[2]}+cars+${inp[0]}-lakh-${inp[1]}-lakh`);
+ await cpage.goto(`https://www.cardekho.com/new-${inp[2]}+cars+${inp[0]}-lakh-${inp[1]}-lakh`);  //link for the website opens through the query
 
 
 
  await cpage.waitFor(3000);
- let count = await cpage.$("#model-highlight");//problem here
+ let count = await cpage.$("#model-highlight");  //gives count of the elements to scraped
 
 if(count==null){
-    count=await cpage.$("span[class='chips']");
+    count=await cpage.$("span[class='chips']");  //this element gives the count if the above element is not present on the website
 }
 
- let loopCount= await cpage.evaluate(function cb(ele) {
+ let loopCount= await cpage.evaluate(function cb(ele) {  //gives the count
     return ele.textContent;
-}
-    , count);
+}, count);
   
    let loop=10;
    if(parseInt(loopCount)){
        loop=parseInt(loopCount)
    }
    else{
- loop=loopCount.split(" ")[2];
+ loop=loopCount.split(" ")[2];  //this count is calculated
    }
 
 if(loop>=10){
-    let Loopcount=Math.ceil(loop/10);
+    let Loopcount=Math.ceil(loop/10);  //no of time the loop works
     console.log(Loopcount);
     await cpage.waitFor(2000);
-     for(let i=0;i<Loopcount;i++){
+     for(let i=0;i<Loopcount;i++){ //this is loop for scrolling till the bottom 
       await scrollToBottom(cpage)
       
         console.log("loaded the spinner");
     }
 }
-    let smList = await cpage.$$(".gsc_col-sm-7.gsc_col-xs-8.gsc_col-md-8.listView.holder");
+        
+let smList = await cpage.$$(".gsc_col-sm-7.gsc_col-xs-8.gsc_col-md-8.listView.holder");
 let cars=[];
-  for(let i=0;i<smList.length;i++){
+        
+        
+  for(let i=0;i<smList.length;i++){  //this whole for loop produces the data for the cars in a well ordered manner
 let val= await cpage.evaluate(function cb(ele) {
     return ele.textContent;
 }
     , smList[i]);
+      
+      
    let idx=val.indexOf("Price");
    let name=val.slice(0,idx);
  
@@ -145,10 +153,11 @@ let val= await cpage.evaluate(function cb(ele) {
    
 
 }
+        
 console.log(cars);
 
 
-function sortIt(a,b){
+function sortIt(a,b){  //this function sort the cars array according to the user
     if(a.t<b.t){
         return a.t-b.t;
     }
@@ -156,14 +165,17 @@ function sortIt(a,b){
        return a.o-b.o;
     }
 }
+        
+        
 cars.sort(sortIt);
 console.log(cars);
 
  
 let ansCars=[]
 for(let i=0;i<cars.length;i++){
-ansCars.push({name:cars[i].name,mileage:cars[i].mileage,engine:cars[i].engine,price:cars[i].price});
+ansCars.push({name:cars[i].name,mileage:cars[i].mileage,engine:cars[i].engine,price:cars[i].price});  //removes the extra variables in the object
 }
+        
 console.table(ansCars);
 writeInFile(ansCars,inp[4]);
 
@@ -221,7 +233,7 @@ await nepage.keyboard.press('Enter');
 })();
 
 
-async function scrollToBottom(page) {
+async function scrollToBottom(page) {//this function is used for scrolling purpose
     const distance = 100; // should be less than or equal to window.innerHeight
     const delay = 700;
     while (await page.evaluate(() => document.scrollingElement.scrollTop + window.innerHeight < document.scrollingElement.scrollHeight)) {
@@ -229,7 +241,7 @@ async function scrollToBottom(page) {
       await page.waitFor(delay);
     }
   }
-function writeInFile(cars,type){
+function writeInFile(cars,type){  //this function helps to write in the file
     
     if(type=='json'){
         
@@ -245,6 +257,7 @@ function writeInFile(cars,type){
 
     }
 }
+    
 function generateTableRow(doc, y, c1, c2, c3, c4) {
     console.log(y,c1,c2,c3,c4);
     doc
@@ -281,7 +294,7 @@ function generateInvoiceTable(doc, invoice) {
       );
     }
   }
-function createInvoice(invoice, path) {
+function createInvoice(invoice, path) { //function for generating the pdf
     let doc = new PDFDocument({ margin: 50 });
   
   
@@ -291,7 +304,7 @@ function createInvoice(invoice, path) {
     doc.end();
     doc.pipe(fs.createWriteStream(path));
   }
-function excelWriter(filePath, json, sheetName) {
+function excelWriter(filePath, json, sheetName) { // function for excel
     // workbook create
     let newWB = xlsx.utils.book_new();
     // worksheet
